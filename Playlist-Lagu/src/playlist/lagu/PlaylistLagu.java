a0430afe4ef363d740f93b974c7789c0a9071eff
syukri124 <syukriihsan680@gmail.com>
2024-10-15 18:20:01 +0700

    Add files via upload

---
+++ Playlist-Lagu/src/playlist/lagu/PlaylistLagu.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package playlist.lagu;
+
+/**
+ *
+ * @author M S I
+ */
+public class PlaylistLagu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
